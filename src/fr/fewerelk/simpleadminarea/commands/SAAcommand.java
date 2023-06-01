75432e45b4c63f1133bb6ac21cf643dde4256c31
FewerElk <gabriel@trouilhet.fr>
2023-06-01 18:56:50 +0200

    somme errors was correctly set.

--- src/fr/fewerelk/simpleadminarea/commands/SAAcommand.java
+++ src/fr/fewerelk/simpleadminarea/commands/SAAcommand.java
@@ -13,25 +13,28 @@ public class SAAcommand implements CommandExecutor {
         if (sender instanceof Player) {
             if (args.length == 0) {
                 sender.sendMessage(ChatColor.GREEN + "---SAA HELP---\n/saa create <area> : create a new area.");
+                return true;
             } else {
                 switch (args[0]) {
                     case "create":
                         SAACreate commandc = new SAACreate();
-                        commandc.onCommand(sender, cmd, label, args);
+                        return commandc.onCommand(sender, cmd, label, args);
                     case "pos1":
                         SAAPos1 command1 = new SAAPos1();
-                        command1.onCommand(sender, cmd, label, args);
+                        return command1.onCommand(sender, cmd, label, args);
                     case "pos2":
                         SAAPos2 command2 = new SAAPos2();
-                        command2.onCommand(sender, cmd, label, args);
+                        return command2.onCommand(sender, cmd, label, args);
                     default:
                         ArgError error = new ArgError();
                         error.argerror(sender.getName());
+                        return false;
                 }
             }
         } else {
-            Bukkit.getLogger().info(ChatColor.GREEN + "---SAA HELP---\n/saa create <area> : create a new area.");
+            ConsoleError error = new ConsoleError();
+            error.consoleError();
+            return false;
         }
-        return true;
     }
 }
